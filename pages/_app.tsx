df25b6b357483a15930e6b8d04c6dea65a9d41fe
Mikkel Folting <folti002@umn.edu>
2023-04-17 12:04:40 -0500

    Sign in added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import 'purecss/build/pure.css';
@@ -7,23 +7,30 @@ import 'purecss/build/pure.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <div className="topHeader">
-        <span>Explore</span>
-      </div>
-      <Component {...pageProps} />
-      <div className="bottomFooter">
-        <div className="pure-g bottomLink" id="explore">
-          <div className="pure-u-1-2">
-            <h2><Link className="bottomLink" href="/">Explore
-            </Link></h2>
-          </div>
+      <SignedIn>
+        <div className="topHeader">
+          <span>Explore</span>
+        </div>
+        <UserButton />
+        <Component {...pageProps} />
+        <div className="bottomFooter">
+          <div className="pure-g bottomLink" id="explore">
+            <div className="pure-u-1-2">
+              <h2><Link className="bottomLink" href="/">Explore
+              </Link></h2>
+            </div>
 
-          <div className="pure-u-1-2 bottomLink" id="mytrips">
-            <h2><Link className="bottomLink" href="/">My Trips
-            </Link></h2>
+            <div className="pure-u-1-2 bottomLink" id="mytrips">
+              <h2><Link className="bottomLink" href="/">My Trips
+              </Link></h2>
+            </div>
           </div>
         </div>
-      </div>
+      </SignedIn>
+
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </ClerkProvider>
   );
 }
