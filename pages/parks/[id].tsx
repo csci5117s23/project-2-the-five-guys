c250c06b94f245088b4e79f900e6221a8a7aafb4
rayrobards <rayrobards12@gmail.com>
2023-04-17 11:59:16 -0500

    filtered parks

--- pages/parks/[id].tsx
+++ pages/parks/[id].tsx
@@ -27,12 +27,13 @@ export default function Home() {
   useEffect(()=> {
     async function loadNationalParkData()
     {
-      let response = await fetch(`https://developer.nps.gov/api/v1/parks`, {
+      let response = await fetch(`https://developer.nps.gov/api/v1/parks?limit=950`, {
         method: 'GET',
         headers: {'x-api-key': `${apiKey}`}
       })
       let data = await response.json();
-      setNationalParks(data.data);
+      let filteredParks = data.data.filter((element) => element.designation.includes("National Park"));
+      setNationalParks(filteredParks);
       setLoading(false);
     }
     loadNationalParkData();
