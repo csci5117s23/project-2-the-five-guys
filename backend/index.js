aac6ac1ab3244be07d2f377aea5ceb900e49e47d
Mikkel Folting <folti002@umn.edu>
2023-04-16 23:27:25 -0500

    Added Codehooks and Clerk

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+
+/*
+* Auto generated Codehooks (c) example
+* Install: npm i codehooks-js codehooks-crudlify
+*/
+import {app} from 'codehooks-js'
+import {crudlify} from 'codehooks-crudlify'
+
+// test route for https://<PROJECTID>.api.codehooks.io/dev/
+app.get('/', (req, res) => {
+  res.send('CRUD server ready')
+})
+
+// Use Crudlify to create a REST API for any collection
+crudlify(app)
+
+// bind to serverless runtime
+export default app.init();
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       setLoading(false);
     }
     loadNationalParkData();
-  },[])
+  },[]);
 
   if(loading)
   {
