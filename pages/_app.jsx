b515a3e2b2b9e6559ce8ab0c8d0948ccc14758a9
Mikkel Folting <folti002@umn.edu>
2023-04-19 12:50:08 -0500

    Added Material UI back into the project

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,51 @@
 import '@/styles/globals.css'
 import { ClerkProvider, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 import 'purecss/build/pure.css';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import {useState} from 'react'
+import MapIcon from "@mui/icons-material/Map";
+import FavoriteIcon from "@mui/icons-material/Favorite"
 
 export default function App({ Component, pageProps }) {
+  const [bottomChoice, setBottomChoice] = useState("");
+  const router = useRouter();
+
+  const navBarSelected={
+    color: 'white',
+    '& .Mui-selected': {color:'white'}
+  };
+  
   return (
     <ClerkProvider {...pageProps}>
       <div className="topHeader">
-        <span>Explore</span>
+        Explore
+        <span className="userButton" ><UserButton/></span>
       </div>
       <UserButton />
       <Component {...pageProps} />
-      <div className="bottomFooter">
-        <div className="pure-g bottomLink" id="explore">
-          <div className="pure-u-1-2">
-            <h2><Link className="bottomLink" href="/">Explore
-            </Link></h2>
-          </div>
-
-          <div className="pure-u-1-2 bottomLink" id="mytrips">
-            <h2>
-              <Link className="bottomLink" href="/">My Trips</Link>
-            </h2>
-          </div>
-        </div>
-      </div>
+      <BottomNavigation showLabels value={bottomChoice} 
+        sx={{
+          bgcolor: 'green',
+          position: 'fixed', 
+          bottom: 0,
+          left: 0, right: 0
+        }}
+        onChange={(event, newValue) => {
+          console.log(newValue);
+          if(newValue == 0){
+            router.push("/");
+          }else{
+            router.push("/");
+          }
+          setBottomChoice(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Explore" icon={<MapIcon />} sx={navBarSelected}/>
+        <BottomNavigationAction label="My Trips" icon={<FavoriteIcon />} sx={navBarSelected}/>
+      </BottomNavigation>
     </ClerkProvider>
   );
 }
