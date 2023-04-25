3e952e162fe60b25295e4143ae9f5d2594386a83
Mikkel Folting <folti002@umn.edu>
2023-04-25 18:53:32 -0500

    Can now search for states as well

--- components/ExploreParkItemList.js
+++ components/ExploreParkItemList.js
@@ -1,15 +1,29 @@
 import ExploreParkItem from "./ExploreParkItem";
 import { ListItem, Grid } from "@mui/material";
+import abbrState from "./GetFullStateName";
 
 export default function ExploreParkItemList(props) {  
   let nationalParks = props.nationalParks;
   const searchValue = props.searchValue.toLowerCase();
 
+  // Callback function to filter parks by both the states they are
+  // located in and by their names.
+  function filterElements(element){
+    // Convert state abbreviations into full names
+    let statesList = element.states.split(',');
+    statesList = statesList.map((state) => {
+      return abbrState(state);
+    });
+
+    return element.name.toLowerCase().includes(searchValue) ||
+            statesList.some(function(elem) {
+              return elem.toLowerCase().includes(searchValue);
+            });
+  }
+
   // Filter national parks by search item
   if(searchValue.length > 0){
-    const result = nationalParks.filter((element) =>
-      element.name.toLowerCase().includes(searchValue)
-    );
+    const result = nationalParks.filter(filterElements);
     nationalParks = result;
   } 
 
