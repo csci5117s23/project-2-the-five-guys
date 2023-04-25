1b7b032e115bab0a5b56f7c79d096b31fa19d845
Nathaniel Ludwig <nathanielludwig1@gmail.com>
2023-04-25 14:52:25 -0500

    add support for date picker

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,18 @@
-import '@/styles/globals.css'
-import { ClerkProvider, SignedIn, UserButton } from '@clerk/nextjs';
-import {useRouter} from 'next/router';
-import 'purecss/build/pure.css';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { Paper, ThemeProvider, createTheme } from '@mui/material';
-import {useState} from 'react'
+import "@/styles/globals.css";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import "purecss/build/pure.css";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import { Paper, ThemeProvider, createTheme } from "@mui/material";
+import { useState } from "react";
 import MapIcon from "@mui/icons-material/Map";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import Image from 'next/image';
-import ParkTrackLogo from '@/public/ParkTrack.png';
-import Head from 'next/head';
+import Image from "next/image";
+import ParkTrackLogo from "@/public/ParkTrack.png";
+import Head from "next/head";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export default function App({ Component, pageProps }) {
   const [bottomChoice, setBottomChoice] = useState(0);
@@ -24,15 +26,15 @@ export default function App({ Component, pageProps }) {
 
   const theme = createTheme({
     palette: {
-      mode: 'light',
+      mode: "light",
       primary: {
-        main: '#4f703b',
+        main: "#387238",
       },
       secondary: {
-        main: '#ef6c00',
+        main: "#ef6c00",
       },
       success: {
-        main: '#85a085',
+        main: "#85a085",
       },
     },
   });
@@ -40,50 +42,61 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <ClerkProvider {...pageProps}>
-        {/* Website title */}
-        <Head>
-          <title> ParkTrack </title>
-        </Head>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          {/* Website title */}
+          <Head>
+            <title> ParkTrack </title>
+          </Head>
 
-        {/* Header */}
-        <div className="topHeader">
-          {/* <Image src={ParkTrackLogo} alt='ParkTrack logo' height={241} width={883} /> */}
-          <Image src={ParkTrackLogo} alt='ParkTrack logo' height={40.2} width={147.2} className='top-logo'/>
-        </div>
+          {/* Header */}
+          <div className="topHeader">
+            {/* <Image src={ParkTrackLogo} alt='ParkTrack logo' height={241} width={883} /> */}
+            <Image
+              src={ParkTrackLogo}
+              alt="ParkTrack logo"
+              height={40.2}
+              width={147.2}
+              className="top-logo"
+            />
+          </div>
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
 
-        {/* Bottom navigation bar */}
-        <Paper
-          sx={{
-            position: 'fixed',
-            zIndex: 100,
-            bottom: 0,
-            left: 0,
-            right: 0,
-          }}
-          elevation={3}
-        >
-          <BottomNavigation
-            // sx={{bgcolor: '#1B742E'}}
-            showLabels
-            value={bottomChoice}
-            onChange={(event, newValue) => {
-                if(newValue == 0){
-                router.push("/parks");
-              } else if(newValue == 1){
-                router.push("/parks");
-              }
-              setBottomChoice(newValue);
+          {/* Bottom navigation bar */}
+          <Paper
+            sx={{
+              position: "fixed",
+              zIndex: 100,
+              bottom: 0,
+              left: 0,
+              right: 0,
             }}
+            elevation={3}
           >
-            <BottomNavigationAction label="Explore" icon={<MapIcon />}/>
-            <BottomNavigationAction label="My Trips" icon={<FavoriteIcon />}/>
-            <SignedIn>
-              <BottomNavigationAction icon={<UserButton />}/>
-            </SignedIn>
-          </BottomNavigation>
-        </Paper>
+            <BottomNavigation
+              // sx={{bgcolor: '#1B742E'}}
+              showLabels
+              value={bottomChoice}
+              onChange={(event, newValue) => {
+                if (newValue == 0) {
+                  router.push("/parks");
+                } else if (newValue == 1) {
+                  router.push("/parks");
+                }
+                setBottomChoice(newValue);
+              }}
+            >
+              <BottomNavigationAction label="Explore" icon={<MapIcon />} />
+              <BottomNavigationAction
+                label="My Trips"
+                icon={<FavoriteIcon />}
+              />
+              <SignedIn>
+                <BottomNavigationAction icon={<UserButton />} />
+              </SignedIn>
+            </BottomNavigation>
+          </Paper>
+        </LocalizationProvider>
       </ClerkProvider>
     </ThemeProvider>
   );
