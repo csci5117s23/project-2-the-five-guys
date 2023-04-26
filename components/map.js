9fffe7fe339c3edf6c9c62eaacbb7688e8461191
rayrobards <rayrobards12@gmail.com>
2023-04-26 14:40:06 -0500

    fixed location button issue i think

--- components/map.js
+++ components/map.js
@@ -6,67 +6,67 @@ import MapIcon from '@/components/mapIcon';
 import ReactDOMServer  from 'react-dom/server';
 import Link from 'next/link';
 import {useState, useEffect} from 'react'
-import { Button, Modal, Box, IconButton} from '@mui/material';
+import { Button, Modal, Box, IconButton, CircularProgress} from '@mui/material';
 import NationalParkItem from '@/components/NationalParkItem';
 import CloseIcon from '@mui/icons-material/Close';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
-
+//button cant be a child of mapcomponent or causes errors, must be a sibling, so separate from map component
+function GoToCurrentLocationButton(props) {
+    //button that goes to current user location on map, keeps same zoom in
+    const {userLocation} = props;
+    const map = useMap();
+    function locationChange()
+    {
+      let userLatLong = [userLocation.coords.latitude, userLocation.coords.longitude];
+      map.flyTo(userLatLong, map._zoom);
+    }
+    return (
+      <Control prepend position='topleft'>
+        <button onClick={() => locationChange()} className='locationButton' color='inherit'>
+          <MyLocationIcon />
+        </button>
+      </Control>
+    )
+  }
 export default function MapComponent(props)
 {
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedPark, setSelectedPark] = useState(null);
   const usLatLongMin = [12, -180];
   const usLatLongMax = [75, -60];
+  const [userLocation, setUserLocation] = useState(null)
   //map bounds
   const bounds = latLngBounds(usLatLongMin, usLatLongMax);
   const {parks} = props;
-
-  //will be set by setUserLocationFunction
-  let userLocation = null;
-
-  function SetUserLocation(){
-    const map = useMap();
-    function success(position) {
-      let userLatLong = [position.coords.latitude, position.coords.longitude];
-      map.setView(userLatLong, 6); 
-      userLocation = position;
-    }
-
-    function error() {
-      console.log("Unable to retrieve your location");
-    }
-
-    if (!navigator.geolocation) {
-      console.log("Geolocation is not supported by your browser");
-    } else {
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-    return null;
+  useEffect(() => {
+    //on success will setUserLocation, otherwise error
+    navigator.geolocation.getCurrentPosition(setUserLocation, error);
+  }, [])
+  function error() {
+    console.log("Unable to retrieve your location");
   }
-   
+  function SetUserLocation() {
+    const map = useMap();
+    let userLatLong = [userLocation.coords.latitude, userLocation.coords.longitude];
+    map.flyTo(userLatLong, 6);
+  }
   function handleOpen(park) {
     setSelectedPark(park);
     setModalOpen(true);
   }
-
-  function GoToCurrentLocation() {
-    //button that goes to current user location on map, keeps same zoom in 
-    const map = useMap();
-    function locationChange() 
-    {
-      let userLatLong = [userLocation.coords.latitude, userLocation.coords.longitude];
-      map.setView(userLatLong, map._zoom); 
-    }
-
-    return (
-      <Control prepend position='topleft'>
-        <button onClick={() => locationChange()} className='locationButton' color='inherit'> 
-          <MyLocationIcon />
-        </button>
-      </Control>
-    )
+  function handleClose() {
+    setModalOpen(false);
+    setSelectedPark(null);
   }
-  
+  if(!userLocation)
+  {
+    return (
+      <div className='centered'>
+        <CircularProgress style={{color: "#1B742E"}}/>
+        <div>Getting Location...</div>
+      </div>
+      )
+  }
   return (
     <>
     <MapContainer className='mapContainer' center={[40, -100]} zoom={6} scrollWheelZoom={true} bounds={bounds} maxBounds={bounds} maxBoundsViscosity={1.0} minZoom={4} maxZoom={8}>
@@ -76,7 +76,7 @@ export default function MapComponent(props)
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GoToCurrentLocation/>
+      <GoToCurrentLocationButton userLocation={userLocation}/>
       {parks.map(park => (
         //custom markers
         <Marker key={park.id} position={[park.latitude, park.longitude]} icon={divIcon({
@@ -89,11 +89,11 @@ export default function MapComponent(props)
            <Button style={{color: "#1B742E"}} onClick={() => handleOpen(park)}>{park.name}</Button>
               <Modal
                   open={modalOpen}
-                  onClose={() => setModalOpen(false)}
+                  onClose={() => handleClose()}
                   aria-labelledby="modal-modal-title"
                   aria-describedby="modal-modal-description">
                   <Box className="modalContentsContainer">
-                   <IconButton aria-label="back" size='large' onClick={() => setModalOpen(false)}>
+                   <IconButton aria-label="back" size='large' onClick={() => handleClose()}>
                       <CloseIcon style={{fontSize: "2rem", color:"#1B742E"}}/>
                    </IconButton>
                     <NationalParkItem nationalPark={selectedPark}></NationalParkItem>
