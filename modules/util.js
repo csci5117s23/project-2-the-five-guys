38dca18a3920b2ea1affd79cf7549cfce60f0d94
Nathaniel Ludwig <nathanielludwig1@gmail.com>
2023-04-26 15:40:55 -0500

    try another fix

--- modules/util.js
+++ modules/util.js
@@ -5,10 +5,6 @@ export function formatDate(isoDateString) {
 
 // From https://github.com/vercel/next.js/discussions/18550#discussioncomment-1248384
 export function avoidRateLimit(delay = 1000) {
-  if (!process.env.IS_BUILD) {
-    return
-  }
-
   return new Promise((resolve) => {
     setTimeout(resolve, delay)
   })
