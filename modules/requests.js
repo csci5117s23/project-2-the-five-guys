7cec55fb754a9a2b8addc00956e8a5be5d41b35b
Nathaniel Ludwig <nathanielludwig1@gmail.com>
2023-04-26 16:50:10 -0500

    try custom user agent

--- modules/requests.js
+++ modules/requests.js
@@ -9,7 +9,8 @@ export async function getNationalParks()
   console.log(apiKey[0]);
   let response = await fetch(`https://developer.nps.gov/api/v1/parks?limit=950`, {
     method: 'GET',
-    headers: {'x-api-key': `${apiKey}`}
+    headers: {'x-api-key': `${apiKey}`},
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
   }).catch((err) => {
       console.log(err);
   });
