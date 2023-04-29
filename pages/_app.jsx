bc0e6e6638c627f2f03f22aef1e777cc42a82924
Nathaniel Ludwig <nathanielludwig1@gmail.com>
2023-04-29 13:02:13 -0500

    use fixed height top bar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/router";
 import "purecss/build/pure.css";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import { Box, Paper, ThemeProvider, createTheme } from "@mui/material";
+import { AppBar, Box, Paper, ThemeProvider, createTheme } from "@mui/material";
 import { useState } from "react";
 import MapIcon from "@mui/icons-material/Map";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Image from "next/image";
 import ParkTrackLogo from "@/public/ParkTrack.png";
 import Head from "next/head";
+import Link from "next/link";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
@@ -48,19 +49,17 @@ export default function App({ Component, pageProps }) {
             <title> ParkTrack </title>
           </Head>
 
-          <Box sx={{ pb: 7 }}>
-            {/* Header */}
-            <div className="topHeader">
-              {/* <Image src={ParkTrackLogo} alt='ParkTrack logo' height={241} width={883} /> */}
+          {/* Header */}
+          <AppBar component="nav" position="sticky" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
+            <Box sx={{py: 0.5}}>
               <Image
                 src={ParkTrackLogo}
                 alt="ParkTrack logo"
-                height={40.2}
-                width={147.2}
-                className="top-logo"
+                height={40}
               />
-            </div>
-
+            </Box>
+          </AppBar>
+          <Box sx={{ pb: 7}}>
             <Component {...pageProps} />
 
             {/* Bottom navigation bar */}
