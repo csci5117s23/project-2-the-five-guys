e639bd17ca42774e2fc7752e02eb4c4799219aaa
John Cullom / cullo006 <johnccullom@gmail.com>
2023-04-30 15:16:02 -0500

    finished share

--- components/ShareComponent.js
+++ components/ShareComponent.js
@@ -12,8 +12,15 @@ export default function ShareComponent(props) {
       if(trip.itinerary){
         trip.itinerary.map(item => {
           let startDate = new Date(item.startDate);
-          let endDate = new Date(item.startDate);
-          itineraryListText += startDate.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" }); + " - " + endDate.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" }); + ": " + item.location + ", " + item.description + "\n"
+          let endDate = new Date(item.endDate);
+          itineraryListText += startDate.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" }) + " - " + endDate.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" });
+          if(item.location){
+            itineraryListText+=": " +item.location;
+          }
+          if(item.description){
+            itineraryListText+=": " +item.description;
+          }
+          itineraryListText += "\n";
         });
       }else{
         itineraryListText = "No itinerary!";
