d18a0140b1d07593fee51575b6075f3ca04343ce
Sahil Raina <saisahil2015@gmail.com>
2023-05-01 09:42:42 -0500

    Added success messages for better UX

--- components/NotesTab.js
+++ components/NotesTab.js
@@ -1,12 +1,18 @@
 import { useAuth } from "@clerk/nextjs";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Snackbar, Alert } from "@mui/material";
 import { useState } from "react";
 import { updateTrip } from "../modules/requests";
 
 export default function NotesTab({ trip, handleUpdates }) {
   const [newNotes, setNewNotes] = useState(trip.notes);
+  const [open, setOpen] = useState(false);
+
+  function handleClose() {
+    setOpen(false);
+  }
   async function handleNotes() {
     try {
+      setOpen(true);
       const response = await handleUpdates(trip._id, { notes: newNotes });
       setNewNotes(response.notes);
     } catch (error) {
@@ -16,7 +22,14 @@ export default function NotesTab({ trip, handleUpdates }) {
   return (
     <>
       <TextField sx={{ my: 2 }} label="Add trip notes here" multiline fullWidth value={newNotes} onChange={(e) => setNewNotes(e.target.value)} />
-      <Button variant="contained" onClick={() => handleNotes()}>Save</Button>
+      <Button variant="contained" onClick={() => handleNotes()}>
+        Save
+      </Button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Success
+        </Alert>
+      </Snackbar>
     </>
   );
 }
